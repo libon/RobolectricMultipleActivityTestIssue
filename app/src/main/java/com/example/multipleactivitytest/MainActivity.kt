40d4dd5658f33e12c53bc42cbc9afa6263083e70
Carmen Alvarez <calvarez@libon.com>
2019-06-12 15:16:53 +0200

    Add a second activity, reachable from a button in the main activity.

--- app/src/main/java/com/example/multipleactivitytest/MainActivity.kt
+++ app/src/main/java/com/example/multipleactivitytest/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.multipleactivitytest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        my_button.setOnClickListener { startActivity(Intent(this, SecondScreenActivity::class.java)) }
     }
 }
